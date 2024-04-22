22f395d922e11efe263bc9b7f171e22783f794ae
Narikorn Phitagragsakul <narikorn_p@flowaccount.com>
2024-04-22 09:32:02 +0000

    fix stg_user_analytics_s3__393

--- models/staging/user_analytics/s3/stg_user_analytics_s3__393_closing_entries.sql
+++ models/staging/user_analytics/s3/stg_user_analytics_s3__393_closing_entries.sql
@@ -1,25 +1,12 @@
 WITH stg_user_analytics_s3__raw_event AS (
-    SELECT
-        {{
-            dbt_utils.star(
-                ref('stg_user_analytics_s3__raw_event'),
-                except=["event_id", "event_name", "event_category"]
-            )
-        }}
-    FROM
-        {{ ref('stg_user_analytics_s3__raw_event') }}
+    SELECT *
+    FROM {{ source('stg_user_analytics_s3', 'stg_user_analytics_s3__raw_event') }}
     WHERE
         event_id = 393
 ),
 
 payload_extracted AS (
-    SELECT
-        {{
-            dbt_utils.star(
-                ref('stg_user_analytics_s3__raw_event'),
-                except=["event_id", "event_name", "event_category", "payload"]
-            )
-        }},
+    SELECT *,
         payload."type"::VARCHAR AS payload_type,
         payload."ClosingPeriod"::INT AS payload_closing_period
     FROM
@@ -27,13 +14,7 @@ payload_extracted AS (
 ),
 
 payload_extracted_2 AS (
-    SELECT
-        {{
-            dbt_utils.star(
-                ref('stg_user_analytics_s3__raw_event'),
-                except=["event_id", "event_name", "event_category", "payload"]
-            )
-        }},
+    SELECT *,
         payload_type,
         payload_closing_period,
         CASE
